80fa940f57f770597947d5bdc75cbb8b65bf2db6
Giorgio Giannotta <jorge_upa@msn.com>
2023-01-14 17:42:41 +0100

    Initial Commit

---
+++ NationalizeName/NationalizeNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NationalizeNameApp.swift
+//  NationalizeName
+//
+//  Created by Giorgio Giannotta on 14/01/23.
+//
+
+import SwiftUI
+
+@main
+struct NationalizeNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
